62d00841ccf6fa0048eafd233d4fda6c8df27d98
Suhan Park <vixx170627@gmail.com>
2021-03-10 18:46:40 +0900

    공공데이터 활용

--- apiUser/routes/api/checkPeople/controller.js
+++ apiUser/routes/api/checkPeople/controller.js
@@ -1,10 +1,11 @@
 const config = require('../../../config')
-var http = require('http')
+var request = require('request')
+
 exports.getPeople = (req,res,next)=>{
     var api_url="https://api.bigdatahub.co.kr/v1/datahub/datasets/search.json"
     // var queryParams = '?' + encodeURIComponent('TDCAccessKey') + '='+encodeURIComponent(config.TDCAccessKey); 
     var queryParams='?'+encodeURIComponent('pid')+'='+encodeURI(req.query.pid)
-    queryParams+='&'+encodeURIComponent('TDCAccessKey')+'='+encodeURIComponent(config.TDCAccessKey)
+    // queryParams+='&'+encodeURIComponent('TDCAccessKey')+'='+encodeURIComponent(config.TDCAccessKey)
     if (req.query.page)
         queryParams+='&$'+encodeURIComponent('page')+'='+encodeURI(req.query.page)
     if (req.query.count)
@@ -16,15 +17,47 @@ exports.getPeople = (req,res,next)=>{
     if (req.query.order)
         queryParams+='&$'+encodeURIComponent('order')+'='+encodeURI(req.query.order)
     api_url+=queryParams
-    console.log(api_url)
     var options={
         url:api_url,
-    }
-    console.log(api_url)
-    var request = require('request')
+        headers:{'TDCAccessKey':config.TDCAccessKey}
+    }    
     request.get(options,(status,response)=>{
+        console.log(options)
         res.json({
             data:response
         })
     })
 }
+
+exports.getAlarm = (req,res,next)=>{
+    var api_url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+(config.PublicKey); /* Service Key*/
+    if(req.query.pageNo)
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(req.query.pageNo); /* */
+    if(req.query.numOfRows)
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(req.query.numOfRows); /* */
+    if(req.query.dataType)
+        queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent(req.query.dataType); /* */
+    if(req.query.base_date)
+        queryParams += '&' + encodeURIComponent('base_date') + '=' + encodeURIComponent(req.query.base_date); /* */
+    if(req.query.base_time)
+        queryParams += '&' + encodeURIComponent('base_time') + '=' + encodeURIComponent(req.query.base_time); /* */
+    if(req.query.nx)
+        queryParams += '&' + encodeURIComponent('nx') + '=' + encodeURIComponent(req.query.nx); /* */
+    if(req.query.ny)
+        queryParams += '&' + encodeURIComponent('ny') + '=' + encodeURIComponent(req.query.ny); /* */
+    console.log(api_url+queryParams)
+    request({
+        url: api_url + queryParams,
+        method: 'GET'
+    }, function (error, response, body) {
+        //console.log('Status', response.statusCode);
+        //console.log('Headers', JSON.stringify(response.headers));
+        //console.log('Reponse received', body);
+        res.json({
+            data:response
+        })
+    });
+    
+}
+
--- apiUser/routes/api/checkPeople/index.js
+++ apiUser/routes/api/checkPeople/index.js
@@ -2,5 +2,5 @@ const router = require('express').Router()
 const controller = require('./controller')
 
 router.get('/getPeople',controller.getPeople)
-
+router.get('/getAlarm',controller.getAlarm)
 module.exports = router
