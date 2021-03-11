947eed0f4d1adbcc53a5e369420733cb2f2e9977
Suhan Park <vixx170627@gmail.com>
2021-03-11 09:06:18 +0900

    공공데이터 코로나 관련 시도(허가가 아직 못 받은 것 같다.), sk 유동인구 데이터 접근 안되는 이유 아직 못찾음

--- apiUser/routes/api/checkPeople/controller.js
+++ apiUser/routes/api/checkPeople/controller.js
@@ -4,7 +4,7 @@ var request = require('request')
 exports.getPeople = (req,res,next)=>{
     var api_url="https://api.bigdatahub.co.kr/v1/datahub/datasets/search.json"
     // var queryParams = '?' + encodeURIComponent('TDCAccessKey') + '='+encodeURIComponent(config.TDCAccessKey); 
-    var queryParams='?'+encodeURIComponent('pid')+'='+encodeURI(req.query.pid)
+    var queryParams='?'+encodeURIComponent('pid')+'='+encodeURIComponent(req.query.pid)
     // queryParams+='&'+encodeURIComponent('TDCAccessKey')+'='+encodeURIComponent(config.TDCAccessKey)
     if (req.query.page)
         queryParams+='&$'+encodeURIComponent('page')+'='+encodeURI(req.query.page)
@@ -19,10 +19,13 @@ exports.getPeople = (req,res,next)=>{
     api_url+=queryParams
     var options={
         url:api_url,
+        method:'GET',
         headers:{'TDCAccessKey':config.TDCAccessKey}
     }    
-    request.get(options,(status,response)=>{
+    request(options,(error,response,body)=>{
         console.log(options)
+        console.log(response)
+        console.log(body)
         res.json({
             data:response
         })
@@ -59,5 +62,32 @@ exports.getAlarm = (req,res,next)=>{
         })
     });
     
+}
+
+exports.getCovid = (req,res,next)=>{
+    var api_url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson';
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+(config.PublicKey); /* Service Key*/
+    if(req.query.pageNo)
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(req.query.pageNo); /* */
+    if(req.query.numOfRows)
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent(req.query.numOfRows); /* */
+    if(req.query.startCreateDt)
+        queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent(req.query.startCreateDt); /* */
+    if(req.query.endCreateDt)
+        queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent(req.query.endCreateDt); /* */
+    
+    api_url+=queryParams
+    console.log(api_url)
+    request({
+        url: api_url,
+        method: 'GET'
+    }, function (error, response, body) {
+        //console.log('Status', response.statusCode);
+        //console.log('Headers', JSON.stringify(response.headers));
+        //console.log('Reponse received', body);
+        res.json({
+            data:response
+        })
+    });
 }
 
--- apiUser/routes/api/checkPeople/index.js
+++ apiUser/routes/api/checkPeople/index.js
@@ -3,4 +3,5 @@ const controller = require('./controller')
 
 router.get('/getPeople',controller.getPeople)
 router.get('/getAlarm',controller.getAlarm)
+router.get('/getCovid',controller.getCovid)
 module.exports = router
